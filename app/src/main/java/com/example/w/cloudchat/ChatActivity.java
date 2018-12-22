b541b24de6160a103e5379b0aa87e7b3ab628368
Shafigullin Mikhail <Shafigullin.M.V@sberbank.ru>
2018-12-22 17:15:44 +0300

    + Activity design

--- app/src/main/java/com/example/w/cloudchat/ChatActivity.java
+++ app/src/main/java/com/example/w/cloudchat/ChatActivity.java
@@ -2,12 +2,28 @@ package com.example.w.cloudchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ChatActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue("ALEXANDER DOLBOEB!");
+            }
+        });
     }
 }
