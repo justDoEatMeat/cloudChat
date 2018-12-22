0e63a48ae71587d416cc31ec879785e8ba070325
justdoeatmeat <justdoeatmeat@mail.ru>
2018-12-22 19:11:03 +0500

    button and edit text

--- app/src/main/java/com/example/w/cloudchat/ChatActivity.java
+++ app/src/main/java/com/example/w/cloudchat/ChatActivity.java
@@ -2,12 +2,47 @@ package com.example.w.cloudchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        button = findViewById(R.id.send_message_b);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //Read from db
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(getClass().getName(),"Value: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError Error) {
+
+            }
+        });
     }
 }
