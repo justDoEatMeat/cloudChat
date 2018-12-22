31b5e4dd8a71f6074534711a82611a57b0b770f4
justdoeatmeat <justdoeatmeat@mail.ru>
2018-12-22 20:13:09 +0500

    1. Blocking to send empty and too long messages. 2. Writing into db

--- app/src/main/java/com/example/w/cloudchat/ChatActivity.java
+++ app/src/main/java/com/example/w/cloudchat/ChatActivity.java
@@ -4,25 +4,81 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class ChatActivity extends AppCompatActivity {
 
+    private static int MAX_MESSAGE_LENGTH = 150;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
-    Button button;
+    DatabaseReference myRef = database.getReference("messages");
+
+    EditText mEditTextMessage;
+    Button mSendButton;
+
+    ArrayList<String> messages = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        mSendButton = findViewById(R.id.send_message_b);
+        mEditTextMessage = findViewById(R.id.message_input);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                myRef.setValue("ALEXANDER DOLBOEB!");
+            public void onClick(View view) {
+                String msg = mEditTextMessage.getText().toString();
+
+                if (msg.equals("")) {
+                    Toast.makeText(getApplicationContext(), "ВВедите сообщение", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (msg.length() == MAX_MESSAGE_LENGTH) {
+                    Toast.makeText(getApplicationContext(), "Слишком длинное сообщение!", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                myRef.push().setValue(msg);
+                mEditTextMessage.setText("");
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String msg = dataSnapshot.getValue(String.class);
+                messages.add(msg);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
